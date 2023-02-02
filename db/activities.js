44d06bf1f17b7df90fc13465c59082113ddb9b5b
Kade Salerno <113953505+ksal44@users.noreply.github.com>
2023-02-02 16:58:01 -0600

    work on activities

--- db/activities.js
+++ db/activities.js
@@ -12,7 +12,7 @@ async function createActivity({ name, description }) {
       returning *;
     `, [name, description]);
 
-    console.log(activity);
+    // console.log(activity);
     console.log("Activity created");
 
     return activity;
@@ -26,11 +26,62 @@ async function createActivity({ name, description }) {
 
 async function getAllActivities() {
   // select and return an array of all activities
+  console.log("Getting all activities...")
+
+  try {
+
+    const {rows} = await client.query(`
+      SELECT * FROM activities;
+    `);
+
+    console.log("Activities got!")
+    // console.log(rows)
+    return rows;
+
+  } catch (error) {
+    console.error("Cannot get  all activities.");
+    throw error;
+  }
+
 }
 
-async function getActivityById(id) {}
+async function getActivityById(id) {
+  console.log("getting activities by id...")
+  try {
+    const {rows: [activity]} = await client.query(`
+    SELECT id, name, description
+    FROM activities
+    WHERE id=$1;
+    `, [id]);
 
-async function getActivityByName(name) {}
+    console.log("Activity got!")
+    console.log(activity)
+    return activity;
+  
+  } catch (error) {
+    console.log("Cannot get activies by id.")
+    throw error;
+  }
+}
+
+async function getActivityByName(name) {
+  console.log("getting activities by name...")
+  try {
+    const { rows: [activity] } = await client.query(`
+    Select id, name, description
+    FROM activities
+    WHERE name=$1;
+    `, [name]);
+
+    console.log("Activity got!")
+    console.log(activity)
+    return activity;
+
+  } catch (error) {
+    console.log("Cannot get activities by name.");
+    throw error
+  }
+}
 
 async function attachActivitiesToRoutines(routines) {
   // select and return an array of all activities
@@ -40,6 +91,16 @@ async function updateActivity({ id, ...fields }) {
   // don't try to update the id
   // do update the name and description
   // return the updated activity
+  const setString = Object.keys(fields).map(
+    (key, index) => `"${key}"=$${index + 1}`
+  ).join(', ');
+
+  try {
+    if (setString.length>0) {
+      const
+    }
+  }
+
 }
 
 module.exports = {
