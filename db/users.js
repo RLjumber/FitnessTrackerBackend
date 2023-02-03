be1248a2f569bdc59b63a7d01aa20466675057b3
PicanteX <mmcintir4@gmail.com>
2023-02-02 20:44:25 -0500

    create user, get user, username, by ID with hashed passwords

--- db/users.js
+++ db/users.js
@@ -1,17 +1,21 @@
 const client = require("./client");
-
+const bcrypt = require('bcrypt')
+const SALT_COUNT = 10;
 // database functions
 
 // user functions
 async function createUser({ username, password }) {
   console.log("Creating user...")
 
+  const hashedPassword = await bcrypt.hash(password, SALT_COUNT)
+  // let userToAdd = {username, hashedPassword }
+
   try {
     const {rows: [user] } = await client.query(`
       INSERT INTO users (username, password)
       values ($1, $2)
       returning *;
-    `, [username, password]);
+    `, [username, hashedPassword]);
 
     // ON CONFLICT (username) DO NOTHING
     // this is optional maybe
@@ -31,14 +35,53 @@ async function createUser({ username, password }) {
 
 async function getUser({ username, password }) {
 
+  const user = await getUserByUsername(username);
+  // console.log(username);
+  // console.log("getting user");
+  // console.log(user);
+  const hashedPassword = user.password;
+
+  let passwordsMatch = await bcrypt.compare(password, hashedPassword) 
+  if (passwordsMatch) {
+    delete user.password;
+    return user;
+    // return the user object (without the password)
+  } else {
+    console.error("passwords do not match");
+    return; 
+  }
 }
 
 async function getUserById(userId) {
+  try{
+    const {rows: [user]} = await client.query(`
+    SELECT * FROM users
+    WHERE id = $1
+    `, [userId]
+    )
+    console.log("attempting to get user by username", user);
+    delete user.password;
+    return user;
+} catch (error) {
+  console.error("Cannot get user by username");
+  throw error;
+}
 
 }
 
 async function getUserByUsername(userName) {
-
+  try{
+    const {rows: [user]} = await client.query(`
+    SELECT * FROM users
+    WHERE username = $1
+    `, [userName]
+    )
+    console.log("attempting to get user by username", user);
+    return user;
+} catch (error) {
+  console.error("Cannot get user by username");
+  throw error;
+}
 }
 
 module.exports = {
