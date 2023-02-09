f381eab21e42dbaae11422335ed821abdce2caea
PicanteX <mmcintir4@gmail.com>
2023-02-08 19:07:51 -0500

    attachActivitiesToRoutines cleanup

--- db/activities.js
+++ db/activities.js
@@ -84,40 +84,36 @@ async function getActivityByName(name) {
 }
 
 async function attachActivitiesToRoutines(routines) {
-  // select and return an array of all activities
-  const routinesToReturn = [...routines];
-  const binds = routines.map((_, index) => `$${index + 1}`).join(", ");
-  const routineIds = routines.map((routine) => routine.id);
-  if (!routineIds?.length) return [];
   try {
-    // get the activities, JOIN with routine_activities (so we can get a routineId), and only those that have those routine ids on the routine_activities join
+    const routineIds = routines.map((routine) => routine.id);
+
+    const setString = routines
+      .map((routine, index) => `$${index + 1}`)
+      .join(", ");
+
     const { rows: activities } = await client.query(
       `
-      SELECT activities.*, routine_activities.duration, routine_activities.count, routine_activities.id AS "routineActivityId", routine_activities."routineId"
-      FROM activities 
-      JOIN routine_activities ON routine_activities."activityId" = activities.id
-      WHERE routine_activities."routineId" IN (${binds});
-    `,
+          SELECT activities.*, routine_activities.duration, routine_activities.count, routine_activities.id AS "routineActivityId", routine_activities."routineId"
+          FROM activities
+          JOIN routine_activities ON routine_activities."activityId" = activities.id
+          WHERE routine_activities."routineId"
+          IN (${setString});
+        `,
       routineIds
     );
 
-    // loop over the routines
-    for (const routine of routinesToReturn) {
-      // filter the activities to only include those that have this routineId
-      const activitiesToAdd = activities.filter(
+    routines.forEach((routine) => {
+      routine.activities = activities.filter(
         (activity) => activity.routineId === routine.id
       );
-      // attach the activities to each single routine
-      routine.activities = activitiesToAdd;
-    }
-    return routinesToReturn;
-  } catch (error) {
-    console.error;
-    throw error;
+    });
+
+    return routines;
+  } catch (e) {
+    console.error(e);
+    throw e;
   }
 }
-
-
 
 async function updateActivity({ id, ...fields}) {
 
